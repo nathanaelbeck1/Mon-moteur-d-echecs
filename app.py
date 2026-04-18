40c21c4e21b65723b90f9f600f653e5ec301448d
Nathanael <Nathanaelbeck1@gmail.com>
2026-04-18 15:59:52 +0200

    render with flask app run

--- app.py
+++ app.py
@@ -520,5 +520,11 @@ def reset():
         "move_stack": []
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
